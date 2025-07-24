6c88b385be0a35c777d38882733f17977a6c1e81
Soni Wadekar <swadekar170@gmail.com>
2025-07-24 22:25:08 +0530

    project setup done

---
+++ moneymanger/src/main/java/com/acts/moneymanger/MoneymangerApplication.java
@@ -0,0 +1,13 @@
+package com.acts.moneymanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneymangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneymangerApplication.class, args);
+	}
+
+}
